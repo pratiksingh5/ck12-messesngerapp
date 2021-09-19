5c35893372af5231d0f9bc8eae91d52e0079014d
Pratik <pratikrajaryan@gmail.com>
2021-09-19 18:54:37 +0530

    make it better

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
 
-mongoose.connect('mongodb://localhost/stwt');
+let uri = 'mongodb+srv://pratiknew:pratik@pratik.7m6lk.mongodb.net/iplpredictor?retryWrites=true&w=majority'
+
+mongoose.connect(uri,{useNewUrlParser:true, useUnifiedTopology:true});
 
 const userSchema = mongoose.Schema({
   username: String,
